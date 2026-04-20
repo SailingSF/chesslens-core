d938485adfe463637b239194188a106fd585dd21
Max <maxabouchar@gmail.com>
2026-04-19 19:10:08 -0700

    update UI

--- ui/views.py
+++ ui/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "ui/index.html")
+    return render(request, "ui/index.html", {"active_page": "home"})
 
 
 def game_review(request):
-    return render(request, "ui/game_review.html")
+    return render(request, "ui/game_review.html", {"active_page": "review"})
 
 
 def position_explorer(request):
-    return render(request, "ui/position_explorer.html")
+    return render(request, "ui/position_explorer.html", {"active_page": "explorer"})
